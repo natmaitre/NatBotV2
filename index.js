20579b1797ddb32670b9c8eb68ffe2615710ad1c
natmaitre <natanvenierfr@gmail.com>
2019-08-18 13:28:58 -0400

    t

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("~/token.json");
+const tokenfile = require("/etc/nat/token.json");
 const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
